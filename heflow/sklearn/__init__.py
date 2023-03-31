785d480638729a67ef5cd4a4f543107d321d6ab7
Elias Koromilas <elias.koromilas@gmail.com>
2023-03-31 11:41:59 +0300

    Clarify scikit-learn model transformations

--- heflow/sklearn/__init__.py
+++ heflow/sklearn/__init__.py
@@ -7,17 +7,19 @@ def log_model(model,
               registered_model_name=None,
               await_registration_for=300):
     if isinstance(model, sklearn.linear_model.LogisticRegression):
-        return heflow.log_model(
-            heflow.sklearn.linear_model.LogisticRegression(model),
-            registered_model_name=registered_model_name,
-            await_registration_for=await_registration_for)
+        model = heflow.sklearn.linear_model.LogisticRegression(model)
     else:
         raise TypeError(f'invalid model instance: {type(model)}')
+
+    return heflow.log_model(model,
+                            registered_model_name=registered_model_name,
+                            await_registration_for=await_registration_for)
 
 
 def save_model(path, model):
     if isinstance(model, sklearn.linear_model.LogisticRegression):
-        heflow.save_model(
-            path, heflow.sklearn.linear_model.LogisticRegression(model))
+        model = heflow.sklearn.linear_model.LogisticRegression(model)
     else:
         raise TypeError(f'invalid model instance: {type(model)}')
+
+    heflow.save_model(path, model)
